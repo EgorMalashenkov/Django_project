c52d116c10d8ca03e0d73fe3e1cc70e8bd2fc0df
Egor <egor_malashenkov@mail.ru>
2021-03-21 00:17:25 +0300

    bug fix

--- tours/views.py
+++ tours/views.py
@@ -1,16 +1,24 @@
+import random
 from django.shortcuts import render
-from data import departures, tours
+from tours import data
 
 
 def main_view(request):
-    return render(request, 'tours/index.html', departures)
+    number = random.randint(0, 30)
+    tours = random.sample(data.tours.items(), 6)
+    return render(request, 'tours/index.html',
+                  context={'title': data.title,
+                           'subtitle': data.subtitle,
+                           'description': data.description,
+                           'tours': tours,
+                           'departures': data.departures})
 
 
 def depature_view(request):
-    return render(request, 'tours/departure.html', {'departures': departures})
+    return render(request, 'tours/departure.html', {'departures': data.departures})
 
 
 def tour_view(request):
-    return render(request, 'tours/tours.html')
+    return render(request, 'tours/tours.html', {'departures': data.departures})
 
 # Create your views here.
